a908a46a3f1884bbcf819b3896aa695cd0a69338
Victor <victor.acero1@gmail.com>
2019-09-09 11:45:26 -0400

    Added LFP (0-100 HZ) filter. So currently have spike and lfp.

--- DataPreperation_Draft1.m
+++ DataPreperation_Draft1.m
@@ -20,15 +20,18 @@ ExtractMode = 4;
 %%
 VoltageData=[]
 [c,d] = butter(6, 2000/(32000/2),'low'); %0 - 2000 Hz filter (butterworth)
-[clow,dlow]=butter(1,4/32000/2,'low');
+[clow,dlow]=butter(6,100/32000/2,'low');
 [cband,dband]=butter(2,[150/32000/2 1500/32000/2],'stop');
 SamplesPerAverage = 5;             % Number of elements to create the mean over
 
 
 
-%%
+%% The other code, e.g. ExtractDataFunction and MasterCode, are trying to do this but for every single electrode. 
 
+%This initial code will give you the filtered (2K Hz low bandpass), and
+%downsample to a 1/5 the # o samples (~6,400)
 
+%This data is for spike analysis 
 for i = 1:23
 
 FileName=[BaseNameCSC,num2str(i),ExtensionnameCSC];
@@ -41,25 +44,55 @@ SpanOfRecording=(eventTimeStamps(7)-eventTimeStamps(6))*1e-6;
 
 %Nlx2MatCSC extracts time stamps and unconverted sample values
 [TimeStampsForSamples, RawVoltageSamples, Header] = Nlx2MatCSC( FileName, FieldSelection, 1, ExtractMode, RangeOfTime);
+
+
 ADConv = str2num(Header{16,1}(end-25:end)); %Pulls out the conversion to obtain voltage
 
-
-
 VoltageData = RawVoltageSamples*ADConv*1e6;% convert matrix to row vector
+
 ReshapedData(1,:)=reshape(VoltageData(:,:),1,[]);
+
 filtered_data = filter(c,d,ReshapedData);
+
 if i==1 %Here we will determine the dimension of and build the Average matrix. 
+
 size = length(filtered_data);      % Find the next smaller multiple of n
+
 m  = size - mod(size, SamplesPerAverage);
-DownsampledData=zeros(64,m/SamplesPerAverage);  
+
+DownsampledData=zeros(64,m/SamplesPerAverage);  %Matrix with 64 electrodes.
+
 end
+
 DataReshapedForAveraging  = reshape(filtered_data(1:m), SamplesPerAverage, []);     % Reshape x to a [n, m/n] matrix
+%1st 3rd 5th 7th .... | This is what the matrix would look like if I wanted
+%2nd 4th 6th 8th ....| to downsample by 2 
 DownsampledData(i,:) = sum(DataReshapedForAveraging, 1) / SamplesPerAverage;  % Calculate the mean over the 1st dim
+
+end
+%%
+
+for i=1:23
+    
+%This will yield the filtered data for LFP detection
+
+filtered_data_LFP = filter(clow,dlow,ReshapedData);
+
+if i==1 %Here we will determine the dimension of and build the Average matrix. 
+
+size = length(filtered_data_LFP);      % Find the next smaller multiple of n
+
+m  = size - mod(size, SamplesPerAverage);
+
+DownsampledData_LFP=zeros(64,m/SamplesPerAverage);  %Matrix with 64 electrodes.
+
 end
 
-
-
-
+DataReshapedForAveraging_LFP  = reshape(filtered_data_LFP(1:m), SamplesPerAverage, []);     % Reshape x to a [n, m/n] matrix
+%1st 3rd 5th 7th .... | This is what the matrix would look like if I wanted
+%2nd 4th 6th 8th ....| to downsample by 2 
+DownsampledData_LFP(i,:) = sum(DataReshapedForAveraging_LFP, 1) / SamplesPerAverage;  % Calculate the mean over the 1st dim
+end
 %% Get correct time stamps 
 tstampsData=TimeStampsForSamples;
 data=RawVoltageSamples;
